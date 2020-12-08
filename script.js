fc0e5ebef9df50e71a99a66f6e66b51de63147d8
Rachel Thompson <thompson.1657@gmail.com>
2020-12-08 15:52:22 -0600

    day is appearing in DOM

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const m = moment();
-console.log(m.format("dddd, MMMM DD"));
+$(document).ready(function () {
+})
+
+var m = moment().format("dddd, MMMM DD");
+var p = $('<p></p>')
+$('span').append(p).text(m)
+
+
+console.log(m);
+
+
+
+
