08edab6a0c157b7653131e8a83df51e449372cb8
Rachel Thompson <thompson.1657@gmail.com>
2020-12-09 15:05:48 -0600

    save clicklistener/reformatting

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
 $(document).ready(function () {
-})
 
-var m = moment().format("dddd, MMMM DD");
-var p = $('<p></p>')
-$('span').append(p).text(m)
+    var m = moment().format("dddd, MMMM DD");
+    var p = $('<p></p>')
+    $('span').append(p).text(m)
+
+    console.log(m);
+
+    $(".saveBtn").on("click", function () {
+
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        console.log('value:', value);
+        console.log('time:', time);
+    });
 
 
-console.log(m);
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
